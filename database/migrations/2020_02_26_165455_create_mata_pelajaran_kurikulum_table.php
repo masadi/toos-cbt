05bdaec0adcfbaffe8ed0b3cb9aca7d13b2395bf
eraporsmk <eraporsmk@gmail.com>
2020-05-09 18:05:46 +0700

    upload source code

--- database/migrations/2020_02_26_165455_create_mata_pelajaran_kurikulum_table.php
+++ database/migrations/2020_02_26_165455_create_mata_pelajaran_kurikulum_table.php
@@ -16,7 +16,7 @@ class CreateMataPelajaranKurikulumTable extends Migration
         Schema::create('mapel_kur', function (Blueprint $table) {
             $table->smallInteger('kurikulum_id');
 			$table->integer('mata_pelajaran_id');
-			$table->decimal('tingkat_pendidikan_id', 2, 0);
+			$table->decimal('tingkat', 2, 0);
 			$table->decimal('jumlah_jam', 2, 0);
             $table->decimal('jumlah_jam_maksimum',2, 0);
 			$table->decimal('wajib',1, 0);
@@ -29,7 +29,7 @@ class CreateMataPelajaranKurikulumTable extends Migration
             $table->timestamp('last_sync')->nullable();
 			$table->foreign('kurikulum_id')->references('kurikulum_id')->on('kurikulum')->onUpdate('CASCADE')->onDelete('CASCADE');
 			$table->foreign('mata_pelajaran_id')->references('mata_pelajaran_id')->on('mata_pelajaran')->onUpdate('CASCADE')->onDelete('CASCADE');
-			$table->primary(['kurikulum_id', 'mata_pelajaran_id', 'tingkat_pendidikan_id']);
+			$table->primary(['kurikulum_id', 'mata_pelajaran_id', 'tingkat']);
         });
     }
 
