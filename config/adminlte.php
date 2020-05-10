dc851f9bee9fee0b8b2e87ce2d80e777d92f70f3
eraporsmk <eraporsmk@gmail.com>
2020-05-11 05:06:28 +0700

    jadwal

--- config/adminlte.php
+++ config/adminlte.php
@@ -423,12 +423,12 @@ return [
                 [
                     'type' => 'js',
                     'asset' => true,
-                    'location' => '/vendor/Datepair.js/src/Datepair.js',
+                    'location' => '/vendor/Datepair.js/dist/Datepair.js',
                 ],
                 [
                     'type' => 'js',
                     'asset' => true,
-                    'location' => '/vendor/Datepair.js/src/jquery.datepair.js',
+                    'location' => '/vendor/Datepair.js/dist/jquery.datepair.js',
                 ],
             ],
         ],
