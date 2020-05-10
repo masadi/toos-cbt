7ca4b7263442b18573d2ded26948c36475e701a1
eraporsmk <eraporsmk@gmail.com>
2020-05-10 19:12:15 +0700

    generate ujian sekolah

--- app/Console/Commands/GenerateUjian.php
+++ app/Console/Commands/GenerateUjian.php
@@ -67,6 +67,34 @@ class GenerateUjian extends Command
                     }
                 }
             }
+        } else {
+            $user = User::where('username', $username)->first();
+            if($user){
+                $all_ujian = Exam::whereHas('pembelajaran', function($query) use ($user){
+                    $query->where('sekolah_id', $user->sekolah_id);
+                })->whereAktif(1)->get();
+                $all_user = User::where('sekolah_id', $user->sekolah_id)->whereNotNull('peserta_didik_id')->get();
+                foreach($all_user as $user){
+                    $this->info('Memperoses peserta '.$user->name);
+                    foreach($all_ujian as $ujian){
+                        $json_file_ujian = 'ujian-'.$user->user_id.'-'.$ujian->exam_id.'.json';
+                        if(!Storage::disk('public')->exists($json_file_ujian)){
+                            $this->info('Mulai proses File Peserta '.$user->name);
+                            $get_ujian = Exam::withCount(['question', 'user_question' => function($query) use ($user){
+                                $query->where('user_questions.user_id', $user->user_id);
+                            }])->with(['question' => function($query){
+                                $query->with('answers');
+                                $query->orderBy('soal_ke');
+                            }, 'user_exam' => function($query) use ($user){
+                                $query->where('user_exams.user_id', $user->user_id);
+                            }])->find($ujian->exam_id);
+                            Storage::disk('public')->put($json_file_ujian, $get_ujian->toJson());
+                        } //else {
+                            //$this->info('File Peserta '.$user->name.' sudah sudah ada');
+                        //}
+                    }
+                }
+            }
         }
     }
 }
