69061c4d23243b6d87d4d4224ceec978b7138d26
eraporsmk <eraporsmk@gmail.com>
2020-05-20 00:28:03 +0700

    force selesai

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -341,7 +341,7 @@ class AjaxController extends Controller
             return $links;
         })
         ->addColumn('force_selesai', function ($item) use ($user){
-            if ($item->status_ujian && $item->updated_at->diffInHours(Carbon::now($user->timezone)) > 2) {
+            if ($item->status_ujian && $item->updated_at->diffInHours(Carbon::now($user->timezone)) > 2 && !$item->force_selesai) {
                 $links = '<a href="'.route('proktor.force_selesai', ['id' => $item->user_exam_id]).'" class="btn btn-sm btn-block btn-danger force_selesai">Force Selesai</a>';
             } elseif(!$item->status_ujian && $item->user_question_count != $item->question_count){
                 $links = '<a href="'.route('proktor.force_selesai', ['id' => $item->user_exam_id]).'" class="btn btn-sm btn-block btn-danger force_selesai">Force Selesai</a>';
