b82e4ffa59a05d596ef5550df61bc05c89ef960a
eraporsmk <eraporsmk@gmail.com>
2020-06-10 11:21:28 +0700

    config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-//use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;
 //use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
@@ -33,16 +33,18 @@ class AppServiceProvider extends ServiceProvider
                 $query->sql . ' [' . implode(', ', $query->bindings) . ']' . PHP_EOL
            );
         });*/
-        if (Schema::hasTable('settings')) {
-            config([
-                'global' => Setting::all([
-                    'key','value'
-                ])->keyBy('key') // key every setting by its name
-                ->transform(function ($setting) {
-                    return $setting->value; // return only the value
-                })
-                ->toArray()
-            ]);
+        if(DB::connection()->getDatabaseName()){
+            if (Schema::hasTable('settings')) {
+                config([
+                    'global' => Setting::all([
+                        'key','value'
+                    ])->keyBy('key') // key every setting by its name
+                    ->transform(function ($setting) {
+                        return $setting->value; // return only the value
+                    })
+                    ->toArray()
+                ]);
+            }
         }
         setlocale(LC_TIME, 'id_ID.utf8');
         Carbon::setLocale(LC_TIME, 'id_ID.utf8');
